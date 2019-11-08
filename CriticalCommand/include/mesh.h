afb80b013030e94b4e70ce2016c49d0b7e329bfe
Zoe Veale <zoeveale@gmail.com>
2019-11-08 13:37:51 -0800

    stencil stuff

--- CriticalCommand/include/mesh.h
+++ CriticalCommand/include/mesh.h
@@ -82,8 +82,10 @@ public:
   }
 
   void DrawStencil(Shader shader) {
-
-
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
+    glActiveTexture(GL_TEXTURE0);
   }
 
 
