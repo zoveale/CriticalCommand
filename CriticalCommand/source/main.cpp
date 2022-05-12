df26ec366f02444041faaf8beb28aefb867b1737
zoveale <zoeveale@gmail.com>
2022-05-12 11:51:42 -0700

    minor changes, git tests

--- CriticalCommand/source/main.cpp
+++ CriticalCommand/source/main.cpp
@@ -1,7 +1,3 @@
-
-
-//#include <iostream>
-
 #include "system.h"
 
 int main(int agrc, char** argv){
@@ -13,23 +9,3 @@ int main(int agrc, char** argv){
   
   return 0;
 }
-
-/*
-int nrAttributes;
-glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
-std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes << std::endl;
-*/
-
-//#define MAX_ACTORS 1<<8
-//  int array[MAX_ACTORS] = { 1,2,3,4,5,6};
-//  array[MAX_ACTORS] = -INT_MAX;
-//  for (int i = 0; i < MAX_ACTORS; i++) {
-//    if (array[i] == -INT_MAX) {
-//      printf("no more actors to pass to shader\n");
-//      break;
-//    }
-//    printf("array[%i] = %i\n", i, array[i]);
-//    
-//  }
-//  printf("for loop has been exited\n");
-//  std::cin.get();
